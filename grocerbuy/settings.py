7614d28eef95b0b1347ac5cc81852a358e3a8a51
Tushar <tushar.dimri22@gmail.com>
2022-12-07 22:57:06 +0530

    Commit

--- grocerbuy/settings.py
+++ grocerbuy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -133,3 +134,13 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP Configutation
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tushar.dimri91@gmail.com'
+EMAIL_HOST_PASSWORD  = 'ycppwfswqgmnffmw'
